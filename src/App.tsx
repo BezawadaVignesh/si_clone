e771444f3f4912a68c64a2506e41c8fe8b1ddc71
BezawadaVignesh <vigneshbezawada3@gmail.com>
2024-01-21 13:48:30 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function App() {
+  const [tsize, setTsize] = useState("text-6xl");
+  return (
+    <div className="flex justify-center items-center w-full h-screen bg-gradient-to-r from-teal-200 to-teal-500">
+      <div className=" text-black">
+        <h1 className={tsize+" font-mono"} onMouseOver={() => setTsize("text-7xl")} onMouseOut={() => setTsize("text-6xl")}>Hello Rahul!</h1>
+      </div>
+    </div>
+  );
+}

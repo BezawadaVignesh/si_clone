7f97f22ea493fd1cf913578228130672b77ad40d
BezawadaVignesh <vigneshbezawada3@gmail.com>
2024-01-30 20:18:50 +0530

    implemented splitpane added editer

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <div className="flex justify-center items-center w-full h-screen bg-gradient-to-r from-teal-200 to-teal-500">
       <div className=" text-black">
-        <h1 className={tsize+" font-mono"} onMouseOver={() => setTsize("text-7xl")} onMouseOut={() => setTsize("text-6xl")}>Hello Rahul!</h1>
+        <h1
+          className={tsize + " font-mono"}
+          onMouseOver={() => setTsize("text-7xl")}
+          onMouseOut={() => setTsize("text-6xl")}
+        >
+          Hello Rahul!
+        </h1>
       </div>
     </div>
   );
